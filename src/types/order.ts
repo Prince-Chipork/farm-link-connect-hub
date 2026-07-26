5f371d253193f80d73f3071cbfb61f2c4acbebb8
Prince-Chipork <chigyodzerterungwa@gmail.com>
2026-07-26 16:11:40 +0100

    Add Order type and OrderStatus definition

--- src/types/order.ts
+++ src/types/order.ts
@@ -1,1 +1,23 @@
+export type OrderStatus =
+  | "Pending"
+  | "Accepted"
+  | "Processing"
+  | "Packed"
+  | "Shipped"
+  | "Delivered"
+  | "Cancelled";
 
+export interface Order {
+  id: string;
+  buyerId: string;
+  items: {
+    productId: string;
+    quantity: number;
+    price: number;
+  }[];
+  total: number;
+  status: OrderStatus;
+  deliveryAddress: string;
+  shippingCost: number;
+  orderDate: string;
+}
