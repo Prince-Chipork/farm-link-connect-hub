3e0d3427935f82b7f38c8dbd4fe185e26e2ab6fa
Prince-Chipork <chigyodzerterungwa@gmail.com>
2026-07-28 05:06:34 +0100

    Update order status return values to title case

--- src/pages/admin/AdminOrderDetails.tsx
+++ src/pages/admin/AdminOrderDetails.tsx
@@ -58,12 +58,12 @@ export default function AdminOrderDetails() {
 
   if (statuses.length === 0) return "pending";
 
-  if (statuses.every((s) => s === "cancelled")) return "cancelled";
-  if (statuses.every((s) => s === "delivered")) return "delivered";
-  if (statuses.every((s) => s === "accepted")) return "accepted";
-  if (statuses.every((s) => s === "processing")) return "processing";
-  if (statuses.every((s) => s === "packed")) return "packed";
-  if (statuses.every((s) => s === "shipped")) return "shipped";
+  if (statuses.every((s) => s === "cancelled")) return "Cancelled";
+  if (statuses.every((s) => s === "delivered")) return "Delivered";
+  if (statuses.every((s) => s === "accepted")) return "Accepted";
+  if (statuses.every((s) => s === "processing")) return "Processing";
+  if (statuses.every((s) => s === "packed")) return "Packed";
+  if (statuses.every((s) => s === "shipped")) return "Shipped";
 
   return "Mixed";
 };
